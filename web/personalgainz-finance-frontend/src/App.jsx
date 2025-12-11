b096aa7cb75044f74cafccb18a8f91c128385eb7
Bobby Davis <bdavis_05@hotmail.com>
2025-12-11 13:18:42 -0600

    Add React frontend inside web/ folder

---
+++ web/personalgainz-finance-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>PersonalGainz Finance</h1>
+      <p>Your frontend is running succesfully!</p>
+    </div>    
+  );
+}
+
+export default App
